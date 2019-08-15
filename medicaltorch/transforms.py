341a6095918980e34d76db58e82a680abcbfe2f3
Diego <diego.vincent.rodriguez@gmail.com>
2019-08-15 16:30:29 +0200

    Bugfix: Skimage only accepts single channel grayscale images

--- medicaltorch/transforms.py
+++ medicaltorch/transforms.py
@@ -699,14 +699,17 @@ class Clahe(MTTransform):
         self.kernel_size = kernel_size
     
     def apply_clahe_to_array(self, array):
-        # NOTE Pytorch standard Image format is [C, W, H]
-        array = np.moveaxis(array, 0, -1) 
+        # NOTE: Skimage grayscale images don't have channels
+        array = np.squeeze(array)
         array = skimage.exposure.equalize_adapthist(
             array,
             kernel_size=self.kernel_size,
             clip_limit=self.clip_limit
         )
-        return np.moveaxis(array, -1, 0)
+
+        # NOTE: Undo the transforms done previously
+        array = np.expand_dims(array, axis=0)
+        return array
 
     def __call__(self, sample):
         processed_dict = {}
